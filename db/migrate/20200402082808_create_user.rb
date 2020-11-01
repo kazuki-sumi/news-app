7d3daf73ab62d88102026d5bc3f660832cd245ce
kazuki-sumi <cs5200@icloud.com>
2020-11-01 09:51:28 +0900

    user_idをoperator_idに修正

--- db/migrate/20200402082808_create_user.rb
+++ db/migrate/20200402082808_create_user.rb
@@ -11,5 +11,19 @@ class CreateUser < ActiveRecord::Migration[6.0]
     end
     add_index :users, :email, :unique => true
     add_index :users, :deleted_at
+
+    create_table :operators do |t|
+      t.string :email, null: false, limit: 191
+      t.string :password_digest, null: false
+      t.integer :role, null: false, default: 0, limit: 2
+      t.string :reset_password_token_digest
+      t.datetime :reset_password_sent_at
+      t.string :remember_digest
+      t.datetime :remember_created_at
+      t.datetime :deleted_at
+      t.timestamps
+    end
+    add_index :operators, :email, :unique => true
+    add_index :operators, :deleted_at
   end
 end
