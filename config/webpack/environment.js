837445d0a2347335ded70f76861d73c111027ecb
kazuki-sumi <cs5200@icloud.com>
2020-11-29 19:12:34 +0900

    vue.js導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 const webpack = require('webpack')
 
 // resolve-url-loader must be used before sass-loader
@@ -15,4 +17,6 @@ environment.plugins.prepend(
     Popper: ['popper.js', 'default']
   })
 )
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
